6fdbcf7ffba7e0cb612296b74f5ad0a82e6867c2
Ose <ose.sunday@gmail.com>
2023-07-24 15:22:34 +0100

    extra deployment files

--- grahouglo_project/wsgi.py
+++ grahouglo_project/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for grahouglo_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +6,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grahouglo_project.settings')
 
 application = get_wsgi_application()
+
+app = application
